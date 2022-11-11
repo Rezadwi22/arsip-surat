29ee854d10ed5dc7ad4031e9c0f1557c05fa83ab
Rezadwi22 <rdwi2266@gmail.com>
2022-11-11 18:30:07 +0700

    arsip

---
+++ application/views/arsip/profil.php
@@ -0,0 +1,15 @@
+<div class="m-3">
+    <h2>About</h2>
+    <div class="row">
+        <div class="col-3">
+            <img src="<?= base_url('assets/ktm.png') ?>" class="img-fluid rounded img-cover-adjust" alt="">
+        </div>
+        <div class="col">
+            <h4>Aplikasi ini dibuat oleh</h4>
+            <p class="m-0">Nama : Reza Dwi agustin</p>
+            <p class="m-0">NIM : 2031730119</p>
+            <p class="m-0">Tanggal : 11 November 2022</p>
+        </div>
+
+    </div>
+</div>
